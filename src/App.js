7d8b4d29770341d4410f8f4150b78a9f02aa4cc7
mobssie <thejurip@gmail.com>
2021-03-22 12:48:37 +0900

    notice write router setting

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-import React, { useState } from 'react';
+import React from 'react';
 import {Route, BrowserRouter, Switch as Router } from "react-router-dom"
 import './styles/style.scss';
 import MainContainer from "./page/main/MainContainer"
 import NoticeListContainer from "./page/boradNotice/NoticeListContainer"
 import NoticeDescContainer from "./page/boradNotice/NoticeDescContainer"
+import NoticeWriteContainer from "./page/boradNotice/NoticeWriteContainer"
 function App() {
   return (
     <BrowserRouter>
+      <Router>
         <Route exact path="/" component={MainContainer} />
         <Route path="/main" component={MainContainer} />
         <Route path="/notice/list" component={NoticeListContainer} />
         <Route path="/notice/desc/:id" component={NoticeDescContainer} />
+        <Route path="/notice/write" component={NoticeWriteContainer} />
+      </Router>
     </BrowserRouter>
   );
 }
