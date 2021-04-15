92c625b0954b0a96570696cb5528b79280bdbf01
mobssie <mobssie.jung@gmail.com>
2021-04-15 15:39:43 +0900

    privacy fix

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import MainDescContainer from "./page/main/MainDescContainer"
 import NoticeListContainer from "./page/boradNotice/NoticeListContainer"
 import NoticeDescContainer from "./page/boradNotice/NoticeDescContainer"
 import NoticeWriteContainer from "./page/boradNotice/NoticeWriteContainer"
+import {TermsPrivacyContainer, TermsPrivacyContainer210415} from "./page/policy"
 function App() {
   return (
     <BrowserRouter>
@@ -16,6 +17,9 @@ function App() {
         <Route path="/notice/list" component={NoticeListContainer} />
         <Route path="/notice/desc/:id" component={NoticeDescContainer} />
         <Route path="/notice/write/ZZzMbZjpPrg" component={NoticeWriteContainer} />
+        <Route path="/terms/privacy" component={TermsPrivacyContainer} />
+        {/* 개정판 */}
+        <Route path="/terms/privacy/210415" component={TermsPrivacyContainer210415} />
       </Router>
     </BrowserRouter>
   );
---
+++ src/page/policy/index.js
@@ -0,0 +1,2 @@
+export { default as TermsPrivacyContainer210415 } from '../policy/privacy/privacy210415'
+export { default as TermsPrivacyContainer } from '../policy/privacy.js'
